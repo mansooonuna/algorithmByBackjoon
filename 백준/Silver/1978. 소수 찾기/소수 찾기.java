fa5693f68768bae38b53beb43cea394329c15ec3
DaSom Park <102853354+mansooonuna@users.noreply.github.com>
2023-03-17 02:39:17 +0900

    [Silver V] Title: 소수 찾기, Time: 128 ms, Memory: 14260 KB -BaekjoonHub

---
+++ 백준/Silver/1978. 소수 찾기/소수 찾기.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        br.readLine();
+        int count = 0;
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        while (st.hasMoreTokens()) {
+            boolean isPrime = true; // 소수 ? true : false
+
+            int num = Integer.parseInt(st.nextToken());
+
+            if (num == 1) {
+                continue;
+            }
+            for (int i = 2; i <= Math.sqrt(num) ; i++) {
+                if(num % i == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+
+            if(isPrime) {
+                count++;
+            }
+        }
+        System.out.println(count);
+        br.close();
+    }
+}
