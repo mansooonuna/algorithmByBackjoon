704a7c112ccd2eaeb2b8cec619a96968b58979ae
DaSom Park <102853354+mansooonuna@users.noreply.github.com>
2023-04-07 16:27:44 +0900

    [Silver IV] Title: 스택, Time: 236 ms, Memory: 21148 KB -BaekjoonHub

--- 백준/Silver/10828. 스택/스택.java
+++ 백준/Silver/10828. 스택/스택.java
@@ -1,50 +1,72 @@
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.OutputStreamWriter;
-import java.util.Scanner;
-import java.util.Stack;
+import java.io.*;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        Scanner sc = new Scanner(System.in);
-        int N = Integer.parseInt(sc.nextLine());
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        Stack<Integer> stack = new Stack<>();
+
+        int N = Integer.parseInt(br.readLine());
+        MyStack stk = new MyStack();
+
         for (int i = 0; i < N; i++) {
-            String command = sc.nextLine();
-            switch (command) {
-                case "pop" :
-                    if (stack.isEmpty()) {
-                        bw.write("-1\n");
-                    } else {
-                        bw.write(stack.pop() + "\n");
-                    }
+            String cmd = br.readLine();
+
+            switch (cmd) {
+                case "pop":
+                    bw.write(stk.pop() + "\n");
                     break;
-                case "size" :
-                    bw.write(stack.size() +"\n");
+                case "size":
+                    bw.write(stk.size()+ "\n");
                     break;
-                case "empty" :
-                    if (stack.isEmpty()) {
-                        bw.write("1\n");
-                    } else {
-                        bw.write("0\n");
-                    }
+                case "empty":
+                    bw.write(stk.empty()+ "\n");
                     break;
-                case "top" :
-                    if (stack.isEmpty()) {
-                        bw.write("-1\n");
-                    } else {
-                        bw.write(stack.peek()+"\n");
-                    }
+                case "top":
+                    bw.write(stk.top()+ "\n");
                     break;
-                default :
-                    String[] array = command.split(" ");
-                    int pushValue = Integer.parseInt(array[1]);
-                    stack.push(pushValue);
+                default:
+                    String[] arr = cmd.split(" ");
+                    stk.push(Integer.parseInt(arr[1]));
                     break;
             }
         }
+
         bw.flush();
         bw.close();
+        br.close();
+
+
+    }
+
+}
+
+class MyStack {
+    ArrayList<Integer> MyStack = new ArrayList<>();
+
+    void push(int x) {
+        this.MyStack.add(x);
+    }
+
+    int pop() {
+        if (!this.MyStack.isEmpty()) {
+            int p = this.MyStack.get(this.MyStack.size() - 1);
+            this.MyStack.remove(this.MyStack.size() - 1);
+            return p;
+        } else return -1;
+    }
+
+    int size() {
+        return this.MyStack.size();
+    }
+
+    int empty() {
+        if (this.MyStack.isEmpty()) return 1;
+        else return 0;
+    }
+
+    int top() {
+        if (this.MyStack.isEmpty()) return -1;
+        else return this.MyStack.get(this.MyStack.size() - 1);
     }
 }
