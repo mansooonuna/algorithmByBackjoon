ac715cef34319cbffac356c82a5ade223446e41b
DaSom Park <102853354+mansooonuna@users.noreply.github.com>
2023-04-07 14:39:51 +0900

    [level 1] Title: 나누어 떨어지는 숫자 배열, Time: 0.79 ms, Memory: 74.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12910. 나누어 떨어지는 숫자 배열/나누어 떨어지는 숫자 배열.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class Solution {
+  public List<Integer> solution(int[] arr, int divisor) {
+        Arrays.sort(arr);
+        
+        List<Integer> answer = new ArrayList<>();
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] % divisor == 0) {
+                answer.add(arr[i]);
+            }
+        }
+        if (answer.isEmpty()) {
+            return Collections.singletonList(-1);
+        }
+        return answer;
+    }
+}
