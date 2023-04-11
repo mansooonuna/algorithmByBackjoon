3cf53dc665e9c6d4b3acb41014434f3f1ad6171d
DaSom Park <102853354+mansooonuna@users.noreply.github.com>
2023-04-11 15:46:51 +0900

    [level 1] Title: 모의고사, Time: 0.59 ms, Memory: 72.7 MB -BaekjoonHub

--- 프로그래머스/lv1/42840. 모의고사/모의고사.java
+++ 프로그래머스/lv1/42840. 모의고사/모의고사.java
@@ -4,7 +4,7 @@ class Solution {
         int[] count = new int[3];
         int[] first = {1,2,3,4,5};
         int[] second = {2,1,2,3,2,4,2,5};
-        int[] third = {3,3,1,1,2,2,4,4,5,5,};
+        int[] third = {3,3,1,1,2,2,4,4,5,5};
 
         for (int i = 0; i < answers.length; i++) {
             if (answers[i] == first[i % first.length]) {
