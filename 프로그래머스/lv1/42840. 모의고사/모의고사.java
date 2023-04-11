3c0e9b7a87b279f8f20d48da4291a315cbce4ff2
DaSom Park <102853354+mansooonuna@users.noreply.github.com>
2023-04-11 09:36:46 +0900

    [level 1] Title: 모의고사, Time: 0.64 ms, Memory: 73.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/42840. 모의고사/모의고사.java
@@ -0,0 +1,38 @@
+import java.util.*;
+class Solution {
+           public int[] solution(int[] answers) {
+        int[] count = new int[3];
+        int[] first = {1,2,3,4,5};
+        int[] second = {2,1,2,3,2,4,2,5};
+        int[] third = {3,3,1,1,2,2,4,4,5,5,};
+
+        for (int i = 0; i < answers.length; i++) {
+            if (answers[i] == first[i % first.length]) {
+                count[0]++;
+            }
+            if (answers[i] == second[i % second.length]) {
+                count[1]++;
+            }
+            if (answers[i] == third[i % third.length]) {
+                count[2]++;
+            }
+        }
+
+        int max = Math.max(Math.max(count[0], count[1]), count[2]);
+
+        ArrayList<Integer> rs = new ArrayList<>();
+
+        for (int i = 0; i < 3; i++) {
+            if(count[i] == max) {
+                rs.add(i + 1);
+            }
+        }
+
+        int[] answer = new int[rs.size()];
+        for (int i = 0; i < answer.length; i++) {
+            answer[i] = rs.get(i);
+        }
+
+        return answer;
+    }
+}
