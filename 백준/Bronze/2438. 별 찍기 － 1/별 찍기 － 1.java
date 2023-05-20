c94ab32c931a8bbdead92891ad35f9e6a1012c0c
DaSom Park <102853354+mansooonuna@users.noreply.github.com>
2023-05-20 20:48:31 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 228 ms, Memory: 14992 KB -BaekjoonHub

---
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.java
@@ -0,0 +1,16 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int A = Integer.parseInt(br.readLine());
+        for (int i = 1; i <= A; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
