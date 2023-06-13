9ec543a0b3bf7f7cc100808247d5c882c9a716af
DaSom Park <102853354+mansooonuna@users.noreply.github.com>
2023-06-13 17:44:33 +0900

    [Bronze V] Title: 아스키 코드, Time: 204 ms, Memory: 17672 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        char c = s.charAt(0);
+        System.out.println((int) c);
+    }
+}
