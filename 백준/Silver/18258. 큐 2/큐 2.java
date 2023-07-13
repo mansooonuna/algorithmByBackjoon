143f64065cf973dec7979080aeb95fdf5a4f9860
DaSom Park <102853354+mansooonuna@users.noreply.github.com>
2023-07-13 10:11:10 +0900

    [Silver IV] Title: 큐 2, Time: 1396 ms, Memory: 474468 KB -BaekjoonHub

--- 백준/Silver/18258. 큐 2/큐 2.java
+++ 백준/Silver/18258. 큐 2/큐 2.java
@@ -1,85 +1,47 @@
 import java.io.*;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringTokenizer st;
         int N = Integer.parseInt(br.readLine());
-        queue q = new queue();
-
+//        Queue<String> queue = new LinkedList<>();
+        List<String> list = new LinkedList<>();
         for (int i = 0; i < N; i++) {
-            st = new StringTokenizer(br.readLine());
-
-            switch (st.nextToken()) {
+            String[] cmd = br.readLine().split(" ");
+            switch (cmd[0]) {
+                case "push":
+                    list.add(cmd[1]);
+                    break;
                 case "pop":
-                    bw.write(q.pop() + "\n");
+                    if (list.isEmpty()) {
+                        bw.write("-1" + "\n");
+                    } else {
+                        bw.write(list.get(0) + "\n");
+                        list.remove(list.get(0));
+                    }
                     break;
                 case "size":
-                    bw.write(q.size() + "\n");
+                    bw.write(list.size() + "\n");
                     break;
                 case "empty":
-                    bw.write(q.empty() + "\n");
+                    if (list.isEmpty()) bw.write("1" + "\n");
+                    else bw.write("0" + "\n");
                     break;
                 case "front":
-                    bw.write(q.front() + "\n");
-                    break;
-                case "back":
-                    bw.write(q.back() + "\n");
+                    if (list.isEmpty()) bw.write("-1" + "\n");
+                    else bw.write(list.get(0) + "\n");
                     break;
                 default:
-                    q.push(Integer.parseInt(st.nextToken()));
+                    if (list.isEmpty()) bw.write("-1" + "\n");
+                    else bw.write(list.get(list.size() - 1) + "\n");
                     break;
             }
         }
         bw.flush();
         bw.close();
-        br.close();
-    }
-
-}
-
-class queue {
-    LinkedList<Integer> q = new LinkedList<>();
-
-    void push(int x) {
-        this.q.add(x);
-    }
-
-    int pop() {
-        if (!this.q.isEmpty()) {
-            int first = this.q.get(0);
-            this.q.remove(0);
-            return first;
-        }
-        return -1;
-    }
-
-    int size() {
-        return this.q.size();
-    }
-
-    int empty() {
-        if (this.q.isEmpty()) {
-            return 1;
-        }
-        return 0;
-    }
-
-    int front() {
-        if (!this.q.isEmpty()) {
-            return this.q.get(0);
-        } else {
-            return -1;
-        }
-    }
-
-    int back() {
-        if (!this.q.isEmpty()) {
-            return this.q.get(this.q.size()-1);
-        } else {
-            return -1;
-        }
     }
 }
