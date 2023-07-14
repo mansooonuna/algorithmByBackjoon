9270af79ce7417d8a5c293805c521dd069519dfc
DaSom Park <102853354+mansooonuna@users.noreply.github.com>
2023-07-14 09:18:44 +0900

    [Silver IV] Title: 덱, Time: 192 ms, Memory: 18604 KB -BaekjoonHub

--- 백준/Silver/10866. 덱/덱.java
+++ 백준/Silver/10866. 덱/덱.java
@@ -1,105 +1,52 @@
 import java.io.*;
-import java.util.*;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.StringTokenizer;
 
 public class Main {
+    static StringBuilder sb = new StringBuilder();
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringTokenizer st;
-        int N = Integer.parseInt(br.readLine());
-        dequeue dq = new dequeue();
 
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int N = Integer.parseInt(br.readLine());
+
+        Deque<String> dequeue = new LinkedList<>();
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
-
             switch (st.nextToken()) {
+                case "push_front":
+                    dequeue.addFirst(st.nextToken());
+                    break;
+                case "push_back":
+                    dequeue.addLast(st.nextToken());
+                    break;
                 case "pop_front":
-                    bw.write(dq.pop_front() + "\n");
+                    if (dequeue.isEmpty()) sb.append("-1").append("\n");
+                    else sb.append(dequeue.pollFirst()).append("\n");
                     break;
                 case "pop_back":
-                    bw.write(dq.pop_back() + "\n");
+                    if (dequeue.isEmpty()) sb.append("-1").append("\n");
+                    else sb.append(dequeue.pollLast()).append("\n");
                     break;
                 case "size":
-                    bw.write(dq.size() + "\n");
+                    sb.append(dequeue.size()).append("\n");
                     break;
                 case "empty":
-                    bw.write(dq.empty() + "\n");
+                    if (dequeue.isEmpty()) sb.append("1").append("\n");
+                    else sb.append("0").append("\n");
                     break;
                 case "front":
-                    bw.write(dq.front() + "\n");
+                    if (dequeue.isEmpty()) sb.append("-1").append("\n");
+                    else sb.append(dequeue.peekFirst()).append("\n");
                     break;
                 case "back":
-                    bw.write(dq.back() + "\n");
+                    if (dequeue.isEmpty()) sb.append("-1").append("\n");
+                    else sb.append(dequeue.peekLast()).append("\n");
                     break;
-                case "push_front":
-                    dq.push_front(Integer.parseInt(st.nextToken()));
-                    break;
-                default:
-                    dq.push_back(Integer.parseInt(st.nextToken()));
-                    break;
-
             }
         }
-        bw.flush();
-        bw.close();
-        br.close();
-    }
-
-}
-
-class dequeue {
-    LinkedList<Integer> dequeue = new LinkedList<>();
-
-    void push_front(int x) {
-        this.dequeue.addFirst(x);
-    }
-
-    void push_back(int x) {
-        this.dequeue.addLast(x);
-    }
-
-    int pop_front() {
-        if (!this.dequeue.isEmpty()) {
-            int first = this.dequeue.get(0);
-            this.dequeue.remove(0);
-            return first;
-        }
-        return -1;
-    }
-
-    int pop_back() {
-        if (!this.dequeue.isEmpty()) {
-            int last = this.dequeue.get(dequeue.size() - 1);
-            this.dequeue.remove(dequeue.size() - 1);
-            return last;
-        }
-        return -1;
-    }
-
-    int size() {
-        return this.dequeue.size();
-    }
-
-    int empty() {
-        if (this.dequeue.isEmpty()) {
-            return 1;
-        }
-        return 0;
-    }
-
-    int front() {
-        if (!this.dequeue.isEmpty()) {
-            return this.dequeue.get(0);
-        } else {
-            return -1;
-        }
-    }
-
-    int back() {
-        if (!this.dequeue.isEmpty()) {
-            return this.dequeue.get(this.dequeue.size() - 1);
-        } else {
-            return -1;
-        }
+        System.out.println(sb);
     }
 }
