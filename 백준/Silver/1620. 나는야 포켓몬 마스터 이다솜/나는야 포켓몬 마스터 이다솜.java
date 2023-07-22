ca82528fd5457179f73fb4486dd7f794b3d88a93
DaSom Park <102853354+mansooonuna@users.noreply.github.com>
2023-07-22 09:19:55 +0900

    [Silver IV] Title: 나는야 포켓몬 마스터 이다솜, Time: 544 ms, Memory: 59008 KB -BaekjoonHub

--- 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.java
+++ 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.java
@@ -1,45 +1,38 @@
-import java.util.*;
-import java.io.*;
-
-/**
- * 나는야 포켓몬 마스터 이다솜
- */
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
         StringTokenizer st = new StringTokenizer(br.readLine());
-
-        int N = Integer.parseInt(st.nextToken());        // 포켓몬의 개수
-        int M = Integer.parseInt(st.nextToken());        // 내가 맞춰야 하는 문제의 개수
-
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
         Map<Integer, String> poketmonsInt = new HashMap<>();
         Map<String, Integer> poketmonsStr = new HashMap<>();
-        for (int i = 1; i <= N; i++) {
+        for (int i = 0; i < N; i++) {
             String name = br.readLine();
-            poketmonsInt.put(i, name);
-            poketmonsStr.put(name, i);
+            poketmonsInt.put(i+1, name);
+            poketmonsStr.put(name, i+1);
         }
-
         for (int i = 0; i < M; i++) {
-            String question = br.readLine();
-
-            if (isNum(question)) {
-                bw.write(poketmonsInt.get(Integer.parseInt(question)) + "\n");
+            String input = br.readLine();
+            if (isNum(input)) {
+                sb.append(poketmonsInt.get(Integer.parseInt(input))).append("\n");
             } else {
-                bw.write(poketmonsStr.get(question) + "\n");
+                sb.append(poketmonsStr.get(input)).append("\n");
             }
         }
-
-        bw.flush();
-        bw.close();
-        br.close();
+        System.out.println(sb);
     }
 
     static boolean isNum(String str) {
         for (int i = 0; i < str.length(); i++) {
-            if ( !Character.isDigit(str.charAt(i))) {
+            if (!Character.isDigit(str.charAt(i))) {
                 return false;
             }
         }
