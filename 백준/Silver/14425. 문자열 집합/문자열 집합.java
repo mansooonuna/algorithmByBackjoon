6f55b1a1bedfe9e668ca675763565b6e9078d32b
DaSom Park <102853354+mansooonuna@users.noreply.github.com>
2023-07-22 09:36:48 +0900

    [Silver III] Title: 문자열 집합, Time: 2724 ms, Memory: 30880 KB -BaekjoonHub

--- 백준/Silver/14425. 문자열 집합/문자열 집합.java
+++ 백준/Silver/14425. 문자열 집합/문자열 집합.java
@@ -8,7 +8,6 @@ import java.util.StringTokenizer;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
         StringTokenizer st = new StringTokenizer(br.readLine());
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
@@ -19,12 +18,12 @@ public class Main {
         int count = 0;
         for (int i = 0; i < M; i++) {
             String input = br.readLine();
-            for (String s : S) {
-                if (input.equals(s)) {
-                    count++;
-                }
+            if (S.contains(input)) {
+                count++;
             }
         }
         System.out.println(count);
     }
+
+
 }
