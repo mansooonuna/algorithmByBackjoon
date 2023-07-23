0dcb4e11b13968caf828656d7194d5cdb18b548a
DaSom Park <102853354+mansooonuna@users.noreply.github.com>
2023-07-23 20:53:37 +0900

    [level 0] Title: 대문자와 소문자, Time: 15.32 ms, Memory: 79.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120893. 대문자와 소문자/대문자와 소문자.java
@@ -0,0 +1,11 @@
+class Solution {
+    public String solution(String my_string) {
+         String answer = "";
+        for (int i = 0; i < my_string.length(); i++) {
+            char c = my_string.charAt(i);
+            if (Character.isLowerCase(c)) answer += Character.toUpperCase(c);
+            else if (Character.isUpperCase(c)) answer += Character.toLowerCase(c);
+        }
+        return answer;
+    }
+}
