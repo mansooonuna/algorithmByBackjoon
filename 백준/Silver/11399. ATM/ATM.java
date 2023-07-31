a8d35fe3af55faf696de1784b339d655525d25b0
DaSom Park <102853354+mansooonuna@users.noreply.github.com>
2023-07-31 09:34:12 +0900

    [Silver IV] Title: ATM, Time: 132 ms, Memory: 14396 KB -BaekjoonHub

--- 백준/Silver/11399. ATM/ATM.java
+++ 백준/Silver/11399. ATM/ATM.java
@@ -15,10 +15,9 @@ public class Main {
         for (int i = 0; i < N; i++) personTime[i] = Integer.parseInt(st.nextToken());
         Arrays.sort(personTime);
 
-        int tmp = 0;
         for (int i = 0; i < N; i++) {
-            totalTime += tmp + personTime[i];
-            tmp += personTime[i];
+            int tmp = N - i;
+            totalTime += personTime[i] * tmp;
         }
 
         System.out.println(totalTime);
