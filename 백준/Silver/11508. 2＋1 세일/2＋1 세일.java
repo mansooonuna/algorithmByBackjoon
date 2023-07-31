b34986435c5cafde4eb358ed1375c2c14011dddc
DaSom Park <102853354+mansooonuna@users.noreply.github.com>
2023-07-31 09:47:33 +0900

    [Silver IV] Title: 2+1 세일, Time: 396 ms, Memory: 26572 KB -BaekjoonHub

--- 백준/Silver/11508. 2＋1 세일/2＋1 세일.java
+++ 백준/Silver/11508. 2＋1 세일/2＋1 세일.java
@@ -3,28 +3,19 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.Collections;
-import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
 
-        Integer[] prices = new Integer[N];
-        for (int i = 0; i < N; i++) {
-            prices[i] = Integer.parseInt(br.readLine());
-        }
+        Integer[] milkPrice = new Integer[N];
+        for (int i = 0; i < N; i++) milkPrice[i] = Integer.parseInt(br.readLine());
+        Arrays.sort(milkPrice, Collections.reverseOrder());
 
-        Arrays.sort(prices, Collections.reverseOrder());
-
-        int sum = 0;
-
-        for (int i = 1; i <= N; i++) {
-            if (i % 3 != 0) { 
-                sum += prices[i - 1];
-            }
-        }
-
-        System.out.println(sum);
+        int minPrice = 0;
+        for (int i = 1; i <= N; i++) if (i % 3 != 0) minPrice += milkPrice[i - 1];
+        
+        System.out.println(minPrice);
     }
 }
