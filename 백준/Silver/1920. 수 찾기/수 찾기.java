3a76e3ceb1e82cae2f9eeca2c2bf19dabdcd2faa
DaSom Park <102853354+mansooonuna@users.noreply.github.com>
2023-08-11 22:40:02 +0900

    [Silver IV] Title: 수 찾기, Time: 684 ms, Memory: 41916 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.java
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -1,38 +1,47 @@
-import java.io.*;
-import java.util.HashMap;
-import java.util.Map;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
+    static int N, M;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-        int N = Integer.parseInt(br.readLine());
+        N = Integer.parseInt(br.readLine());
+        int[] A = new int[N];
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int[] nums = new int[N];
         for (int i = 0; i < N; i++) {
-            nums[i] = Integer.parseInt(st.nextToken());
+            A[i] = Integer.parseInt(st.nextToken());
         }
+        Arrays.sort(A);
 
-        int M = Integer.parseInt(br.readLine());
+        M = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
         StringTokenizer st2 = new StringTokenizer(br.readLine());
-        int[] ans = new int[M];
         for (int i = 0; i < M; i++) {
             int num = Integer.parseInt(st2.nextToken());
-            for (int num1 : nums) {
-                if (num == num1) {
-                    ans[i] = 1;
-                    break;
-                }
-            }
+
+            if (binarySearch(A, num)) sb.append("1").append("\n");
+            else sb.append("0").append("\n");
         }
 
-        for (int num : ans) {
-            bw.write(num + " ");
-        }
+        System.out.println(sb);
+    }
 
-        bw.flush();
-        bw.close();
+    private static boolean binarySearch(int[] arr, int num) {
+        int leftIdx = 0;
+        int rightIdx = N - 1;
+
+        while (leftIdx <= rightIdx) {
+            int midIdx = (leftIdx + rightIdx) / 2;
+            int mid = arr[midIdx];
+
+            if (num < mid) rightIdx = midIdx - 1;
+            else if (num > mid) leftIdx = midIdx + 1;
+            else return true;
+        }
+        return false;
     }
 }
