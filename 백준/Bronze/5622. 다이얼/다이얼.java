0ab1cc5a5453440a656a83e5db1da57a29c59ce7
DaSom Park <102853354+mansooonuna@users.noreply.github.com>
2023-08-15 21:15:09 +0900

    [Bronze II] Title: 다이얼, Time: 124 ms, Memory: 14300 KB -BaekjoonHub

---
+++ 백준/Bronze/5622. 다이얼/다이얼.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String word = br.readLine().toUpperCase();
+
+        int time = 0;
+        for (int i = 0; i < word.length(); i++) {
+            char c = word.charAt(i);
+            if (c >= 'A' && c <= 'C') time += 3;
+            else if (c >= 'D' && c <= 'F') time += 4;
+            else if (c >= 'G' && c <= 'I') time += 5;
+            else if (c >= 'J' && c <= 'L') time += 6;
+            else if (c >= 'M' && c <= 'O') time += 7;
+            else if (c >= 'P' && c <= 'S') time += 8;
+            else if (c >= 'T' && c <= 'V') time += 9;
+            else if (c >= 'W' && c <= 'Z') time += 10;
+        }
+
+        System.out.println(time);
+    }
+}
