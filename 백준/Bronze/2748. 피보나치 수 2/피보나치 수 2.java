ad315bde0794022c76b59b3000eaeb3e3997424d
DaSom Park <102853354+mansooonuna@users.noreply.github.com>
2023-09-20 09:59:31 +0900

    [Bronze I] Title: 피보나치 수 2, Time: 124 ms, Memory: 14228 KB -BaekjoonHub

---
+++ 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        System.out.println(fibonacci(n));
+    }
+
+    private static long fibonacci(int n) {
+        long[] arr = new long[n + 1];
+        arr[0] = 0;
+        arr[1] = 1;
+        for (int i = 2; i < n + 1 ; i++) {
+            arr[i] = arr[i - 1] + arr[i - 2];
+        }
+        return arr[n];
+    }
+}
