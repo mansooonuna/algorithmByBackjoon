b25982dbace8b7f6a0e3d88ddc526b081db10682
DaSom Park <102853354+mansooonuna@users.noreply.github.com>
2023-10-25 01:08:16 +0900

    [Silver IV] Title: 파스칼 삼각형, Time: 124 ms, Memory: 14196 KB -BaekjoonHub

---
+++ 백준/Silver/15489. 파스칼 삼각형/파스칼 삼각형.java
@@ -0,0 +1,46 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+class Main {
+    static int[][] dp;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int R = Integer.parseInt(st.nextToken());
+        int C = Integer.parseInt(st.nextToken());
+        int W = Integer.parseInt(st.nextToken());
+
+        dp = new int[31][31];
+        dp[1][1] = dp[2][1] = dp[2][2] = 1;
+
+        for (int i = 3; i <= 30; i++) {
+            dp[i][1] = 1;
+            for (int j = 2; j < i; j++) {
+                dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j];
+            }
+            dp[i][i] = 1;
+        }
+
+        int sum = 0;
+        for (int i = 0; i < W; i++) {
+            for (int j = 0; j < i + 1; j++) {
+                sum += dp[R + i][C + j];
+            }
+        }
+        System.out.println(sum);
+    }
+}
+
+/**
+ * 1 0 0 0
+ * 1 1 0 0
+ * 1 2 1 0
+ * 1 3 3 1
+ * ...
+ * <p>
+ * dp[i][j] = i번째 줄 j번째 수
+ * dp[i][j] = dp[i-1][j-1] + dp[i-1][j]
+ */
