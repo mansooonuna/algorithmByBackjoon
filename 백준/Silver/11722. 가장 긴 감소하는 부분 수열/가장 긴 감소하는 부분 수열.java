f6d641d3f90bf0705ccaffed6da468403f7f6f07
DaSom Park <102853354+mansooonuna@users.noreply.github.com>
2023-11-02 18:40:44 +0900

    [Silver II] Title: 가장 긴 감소하는 부분 수열, Time: 148 ms, Memory: 14508 KB -BaekjoonHub

---
+++ 백준/Silver/11722. 가장 긴 감소하는 부분 수열/가장 긴 감소하는 부분 수열.java
@@ -0,0 +1,42 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n + 1];
+        int[] dp = new int[n + 1];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 1; i <= n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        dp[1] = 1;
+
+        for (int i = 2; i <= n; i++) {
+            dp[i] = 1;
+            for (int j = 0; j < i; j++) {
+                if (arr[i] < arr[j] && dp[i] <= dp[j]) dp[i] = dp[j] + 1;
+                else if (arr[i] == arr[j]) dp[i] = dp[j];
+            }
+        }
+
+        int max = 0;
+        for (int i = 1; i <= n; i++) {
+            max = Math.max(dp[i], max);
+        }
+        System.out.println(max);
+    }
+}
+
+
+/*
+10 30 10 20 20 10
+1  1  2  2  2  3
+
+dp[i] = 앞 숫자가 뒤 숫자보다 크다면 dp[i-1]++ / 아니면 dp[i-1]
+ */
