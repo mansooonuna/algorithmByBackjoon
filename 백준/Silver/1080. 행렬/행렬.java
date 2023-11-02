520beba7fa12b0c3fbfea8cbe873d1a85a3a12a7
DaSom Park <102853354+mansooonuna@users.noreply.github.com>
2023-11-02 15:05:48 +0900

    [Silver I] Title: 행렬, Time: 156 ms, Memory: 15396 KB -BaekjoonHub

---
+++ 백준/Silver/1080. 행렬/행렬.java
@@ -0,0 +1,70 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int N, M;
+    static int[][] A, B;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        A = new int[N][M];
+        B = new int[N][M];
+
+        for (int i = 0; i < N; i++) {
+            String[] input = br.readLine().split("");
+            for (int j = 0; j < M; j++) {
+                A[i][j] = Integer.parseInt(input[j]);
+            }
+        }
+
+        for (int i = 0; i < N; i++) {
+            String[] input = br.readLine().split("");
+            for (int j = 0; j < M; j++) {
+                B[i][j] = Integer.parseInt(input[j]);
+            }
+        }
+
+        int answer = 0;
+        // 3*3 뒤집어야하기 때문에 경계는 N-2, M-2까지
+        for (int i = 0; i < N - 2; i++) {
+            for (int j = 0; j < M - 2; j++) {
+                if (A[i][j] != B[i][j]) {
+                    flip(i, j);
+                    answer++;
+                }
+            }
+        }
+
+        if (isSame()) {
+            System.out.println(answer);
+        } else {
+            System.out.println("-1");
+        }
+    }
+
+    // 3*3 뒤집기
+    static void flip(int x, int y) {
+        for (int i = x; i < x + 3; i++) {
+            for (int j = y; j < y + 3; j++) {
+                A[i][j] = 1 - A[i][j];
+            }
+        }
+    }
+
+    // A-B 두 행렬이 같은지?
+    static boolean isSame() {
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                if (A[i][j] != B[i][j]) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
