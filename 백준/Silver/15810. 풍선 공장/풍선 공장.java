f41fa41dbe72bbdee0c5ff4fbca7e2572d04f52c
DaSom Park <102853354+mansooonuna@users.noreply.github.com>
2023-11-09 20:40:20 +0900

    [Silver II] Title: 풍선 공장, Time: 696 ms, Memory: 92248 KB -BaekjoonHub

---
+++ 백준/Silver/15810. 풍선 공장/풍선 공장.java
@@ -0,0 +1,49 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken()); // 스태프 수
+        int M = Integer.parseInt(st.nextToken()); // 풍선 개수
+
+        int[] times = new int[N];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            times[i] = Integer.parseInt(st.nextToken());
+        }
+
+        long left = 0;
+        long right = 1_000_000L * M; // 제일 오래 걸리는 시간 * 필요한 풍선 개수
+
+        long rs = 0;
+        while (left <= right) {
+            long mid = (left + right) / 2;
+
+            long total = 0;
+            for (int i = 0; i < N; i++) {
+                total += mid / times[i];
+            }
+
+            if (total >= M) {
+                rs = mid;
+                right = mid - 1;
+            } else left = mid + 1;
+        }
+
+        System.out.println(rs);
+    }
+}
+
+/*
+  1 2 3 4 5 6 7 8 9 10 11 12 13 (14) 15
+5         1         2                3
+7             1                  2
+3     1     2     3       4          5
+
+최대 시간 1_000_000 * 1_000_000 > int
+그래서 long으로 선언
+ */
