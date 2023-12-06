6c80d852b0d8f2005ba99b3072f477f0332482cd
DaSom Park <102853354+mansooonuna@users.noreply.github.com>
2023-12-06 15:01:00 +0900

    [Bronze II] Title: Hashing, Time: 124 ms, Memory: 14188 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.java
+++ 백준/Bronze/15829. Hashing/Hashing.java
@@ -1,21 +1,23 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    static final int M = 1234567891;
+    static final int r = 31;
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int L = Integer.parseInt(br.readLine());
         String input = br.readLine();
 
-        long ans = 0;
+        long answer = 0;
         long r_pow = 1;
+
         for (int i = 0; i < L; i++) {
             int alphabet = input.charAt(i) - 'a' + 1;
-            ans += alphabet * r_pow;
-            r_pow *= 31;
+            answer = (answer + (alphabet * r_pow) % M) % M;
+            r_pow = (r_pow * r) % M;
         }
 
-        System.out.println(ans);
+        System.out.println(answer);
     }
 }
