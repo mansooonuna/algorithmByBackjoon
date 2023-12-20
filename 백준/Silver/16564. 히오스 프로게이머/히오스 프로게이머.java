977423f844fbfea86be4dfdd87aa9fd4727bf2ff
DaSom Park <102853354+mansooonuna@users.noreply.github.com>
2023-12-20 09:11:03 +0900

    [Silver I] Title: 히오스 프로게이머, Time: 284 ms, Memory: 22972 KB -BaekjoonHub

--- 백준/Silver/16564. 히오스 프로게이머/히오스 프로게이머.java
+++ 백준/Silver/16564. 히오스 프로게이머/히오스 프로게이머.java
@@ -1,7 +1,6 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -11,24 +10,24 @@ public class Main {
         int N = Integer.parseInt(st.nextToken());
         int K = Integer.parseInt(st.nextToken());
 
+        int start = Integer.MAX_VALUE; // 최소 레벨부터
+
         int[] level = new int[N];
         for (int i = 0; i < N; i++) {
             level[i] = Integer.parseInt(br.readLine());
+            start = Math.min(start, level[i]);
         }
 
-        Arrays.sort(level);
-
-        int start = level[0]; // 최소 레벨부터
-        int end = level[0] + K; // 최소 레벨 + K 까지
+        int end = start + K; // 최소 레벨 + K 까지
 
         while (start <= end) {
             int mid = (start + end) / 2;
             long total = 0;
-            for (int i = 0; i < N; i++) {
-                if (level[i] < mid) { // mid보다 작은 레벨만
-                    total += mid - level[i]; // 필요한 레벨을 total에 저장
+ 
+                for (int lv : level) {
+                    if (lv < mid) total += mid - lv;
                 }
-            }
+    
 
             if (total > K) end = mid - 1;
             else start = mid + 1;
