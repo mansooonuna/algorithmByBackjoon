2e0a689c3ae091714a27670b593f855adea2afba
DaSom Park <102853354+mansooonuna@users.noreply.github.com>
2023-12-21 17:22:16 +0900

    [Gold V] Title: 1, 2, 3 더하기 4, Time: 128 ms, Memory: 14620 KB -BaekjoonHub

---
+++ 백준/Gold/15989. 1， 2， 3 더하기 4/1， 2， 3 더하기 4.java
@@ -0,0 +1,56 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+        int[][] dp = new int[10001][4];
+
+        dp[1][1] = dp[2][1] = dp[2][2] = dp[3][1] = dp[3][2] = dp[3][3] = 1;
+
+        for (int i = 4; i < 10001; i++) {
+            dp[i][1] = dp[i - 1][1];
+            dp[i][2] = dp[i - 2][1] + dp[i - 2][2];
+            dp[i][3] = dp[i - 3][1] + dp[i - 3][2] + dp[i - 3][3];
+        }
+
+        StringBuilder sb = new StringBuilder();
+        while (T-- > 0) {
+            int n = Integer.parseInt(br.readLine());
+            sb.append(dp[n][1] + dp[n][2] + dp[n][3]).append("\n");
+        }
+
+        System.out.println(sb);
+    }
+}
+
+/*
+정수 n
+dp[n][1] = 1로 끝나는 것
+dp[n][2] = 2로 끝나는 것
+dp[n][3] = 3로 끝나는 것
+
+n = 1
+[1][1] = 1
+
+n = 2
+[2][1] = 1 + 1 = 1
+[2][2] = 2 = 1
+
+n = 3
+[3][1] = 1 + 1 + 1 = 1
+[3][2] = 1 + 2 = 1
+[3][3] = 1
+
+n = 4
+[4][1] = 1 + 1 + 1 + 1 = 2 + 1 + 1 = 2
+[4][2] = 2 + 2 = 1
+[4][3] = 1 + 3 = 1
+...
+
+dp[n][1] = dp[n - 1][1];
+dp[n][2] = dp[n - 2][1] + dp[n - 2][2];
+dp[n][3] = dp[n - 3][1] + dp[n - 3][2] + dp[n - 3][3];
+ */
