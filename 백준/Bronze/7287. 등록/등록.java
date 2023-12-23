a57e2a58bb27193d5b92ff67d4e49e936bd2d1f7
DaSom Park <102853354+mansooonuna@users.noreply.github.com>
2023-12-23 21:02:10 +0900

    [Bronze V] Title: 등록, Time: 112 ms, Memory: 13960 KB -BaekjoonHub

---
+++ 백준/Bronze/7287. 등록/등록.java
@@ -0,0 +1,8 @@
+import java.io.IOException;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        System.out.println(320);
+        System.out.println("parkdasom95");
+    }
+}
