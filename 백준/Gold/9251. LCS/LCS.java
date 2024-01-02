68cf8a6bc8ded1c4ca5266bd8c99f80c3fddfa5f
DaSom Park <102853354+mansooonuna@users.noreply.github.com>
2024-01-02 14:26:40 +0900

    [Gold V] Title: LCS, Time: 144 ms, Memory: 18248 KB -BaekjoonHub

---
+++ 백준/Gold/9251. LCS/LCS.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        char[] input1 = br.readLine().toCharArray();
+        char[] input2 = br.readLine().toCharArray();
+        int[][] dp = new int[input1.length + 1][input2.length + 1];
+
+        for (int i = 1; i <= input1.length; i++) {
+            for (int j = 1; j <= input2.length; j++) {
+                if (input1[i - 1] == input2[j - 1]) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                } else {
+                    dp[i][j] = Math.max(dp[i][j - 1], dp[i - 1][j]);
+                }
+            }
+        }
+
+        System.out.println(dp[input1.length][input2.length]);
+    }
+}
